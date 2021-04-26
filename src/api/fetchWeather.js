fc920ae6e9c0744495bda44e8194194c8e215cae
Bruno Montebello <brunormontebello@hotmail.com>
2021-04-26 09:48:58 +0100

    Set React components

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'e09d948d430987a6ee6b45d377aa9ce4';
 
-export const fecthWeather = async (query) => {
+export const fetchWeather = async (query) => {
   const { data } = await axios.get(URL, {
     params: {
       q: query,
