820cb701f8af7a1e8ee820e2f03869062cd08986
Bruno Montebello <brunormontebello@hotmail.com>
2021-04-26 08:41:20 +0100

    Add axios + set api up

---
+++ src/api/fecthWeather.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'e09d948d430987a6ee6b45d377aa9ce4';
+
+export const fecthWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: 'metric',
+      APPID: API_KEY,
+    }
+  });
+
+  return data;
+}
